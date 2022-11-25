9250c96f80fb77b4aaaff7e622ceeeacc6270aa4
Peter Nijem <peter.nijem@gmail.com>
2022-11-25 16:25:38 +0200

    Update project to Java 11 and pass bytes value

--- src/main/java/be/jovacon/kafka/connect/MQTTSourceConverter.java
+++ src/main/java/be/jovacon/kafka/connect/MQTTSourceConverter.java
@@ -1,6 +1,7 @@
 package be.jovacon.kafka.connect;
 
 import be.jovacon.kafka.connect.config.MQTTSourceConnectorConfig;
+import java.util.HashMap;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.header.ConnectHeaders;
 import org.apache.kafka.connect.source.SourceRecord;
@@ -8,49 +9,39 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-
 /**
  * Converts a MQTT message to a Kafka message
  */
 public class MQTTSourceConverter {
 
-    private MQTTSourceConnectorConfig mqttSourceConnectorConfig;
+  private MQTTSourceConnectorConfig mqttSourceConnectorConfig;
 
-    private Logger log = LoggerFactory.getLogger(MQTTSourceConverter.class);
+  private Logger log = LoggerFactory.getLogger(MQTTSourceConverter.class);
 
-    public MQTTSourceConverter(MQTTSourceConnectorConfig mqttSourceConnectorConfig) {
-        this.mqttSourceConnectorConfig = mqttSourceConnectorConfig;
-    }
+  public MQTTSourceConverter(MQTTSourceConnectorConfig mqttSourceConnectorConfig) {
+    this.mqttSourceConnectorConfig = mqttSourceConnectorConfig;
+  }
 
-    protected SourceRecord convert(String topic, MqttMessage mqttMessage) {
-        log.debug("Converting MQTT message: " + mqttMessage);
-        // Kafka 2.3
-        ConnectHeaders headers = new ConnectHeaders();
-        headers.addInt("mqtt.message.id", mqttMessage.getId());
-        headers.addInt("mqtt.message.qos", mqttMessage.getQos());
-        headers.addBoolean("mqtt.message.duplicate", mqttMessage.isDuplicate());
+  protected SourceRecord convert(String topic, MqttMessage mqttMessage) {
+    log.debug("Converting MQTT message: " + mqttMessage);
+    // Kafka 2.3
+    ConnectHeaders headers = new ConnectHeaders();
+    headers.addInt("mqtt.message.id", mqttMessage.getId());
+    headers.addInt("mqtt.message.qos", mqttMessage.getQos());
+    headers.addBoolean("mqtt.message.duplicate", mqttMessage.isDuplicate());
 
-        // Kafka 1.0
-        /*SourceRecord sourceRecord = new SourceRecord(
-                new HashMap<>(),
-                new HashMap<>(),
-                this.mqttSourceConnectorConfig.getString(MQTTSourceConnectorConfig.KAFKA_TOPIC),
-                Schema.STRING_SCHEMA,
-                new String(mqttMessage.getPayload()));
-*/
-        // Kafka 2.3
-        SourceRecord sourceRecord = new SourceRecord(new HashMap<>(),
-                new HashMap<>(),
-                this.mqttSourceConnectorConfig.getString(MQTTSourceConnectorConfig.KAFKA_TOPIC),
-                (Integer) null,
-                Schema.STRING_SCHEMA,
-                topic,
-                Schema.STRING_SCHEMA,
-                new String(mqttMessage.getPayload()),
-                System.currentTimeMillis(),
-                headers);
-        log.debug("Converted MQTT Message: " + sourceRecord);
-        return sourceRecord;
-    }
+    // Kafka 2.3
+    SourceRecord sourceRecord = new SourceRecord(new HashMap<>(),
+        new HashMap<>(),
+        this.mqttSourceConnectorConfig.getString(MQTTSourceConnectorConfig.KAFKA_TOPIC),
+        null,
+        Schema.BYTES_SCHEMA,
+        topic,
+        Schema.BYTES_SCHEMA,
+        mqttMessage.getPayload(),
+        System.currentTimeMillis(),
+        headers);
+    log.debug("Converted MQTT Message: " + sourceRecord);
+    return sourceRecord;
+  }
 }
