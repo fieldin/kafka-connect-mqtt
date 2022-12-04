4cc426baf207a3f42c4412b7c53c2305e0f75a19
Peter Nijem <peter.nijem@gmail.com>
2022-12-04 13:03:06 +0200

    update script

--- src/main/resources/loadSourceConnector.sh
+++ src/main/resources/loadSourceConnector.sh
@@ -5,10 +5,10 @@ curl -X POST \
     "config":
     {
       "connector.class":"be.jovacon.kafka.connect.MQTTSourceConnector",
-      "mqtt.topic":"my_mqtt_topic",
-      "kafka.topic":"my_kafka_topic",
-      "mqtt.clientID":"my_client_id",
-      "mqtt.broker":"tcp://127.0.0.1:1883",
+      "mqtt.topic":"<my_mqtt_topic>",
+      "kafka.topic":"<my_kafka_topic>",
+      "mqtt.clientID":"<my_client_id>",
+      "mqtt.broker":"tcp://<mqtt-borker>:1883",
       "key.converter":"org.apache.kafka.connect.storage.StringConverter",
       "key.converter.schemas.enable":"false",
       "value.converter":"org.apache.kafka.connect.converters.ByteArrayConverter",
